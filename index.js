6e928a15f42ac6d35446d0ca27a79e021349680c
progmamun <mamun333089@gmail.com>
2022-04-21 01:11:17 +0600

    connect-mongodb

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB
+const uri =
+  'mongodb+srv://mongoadmin:3RVxVRm3ZGIV8b9R@cluster0.njs0j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect(err => {
+  const collection = client.db('test').collection('devices');
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Running My Node CRUD Server');
