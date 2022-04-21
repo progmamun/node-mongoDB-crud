111da898b2c6a9b3469a025271f133d4a4eb3974
progmamun <mamun333089@gmail.com>
2022-04-21 14:30:10 +0600

    get-response-client

--- react-mongodb-crud/src/components/AddUser/AddUser.jsx
+++ react-mongodb-crud/src/components/AddUser/AddUser.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
 const AddUser = () => {
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email };
+
+    // send data to the server
+    fetch('', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  };
   return (
     <div>
-      <h2>add user</h2>
+      <h2> Please add a new User</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+      </form>
     </div>
   );
 };
