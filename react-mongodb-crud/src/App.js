05e5fb7b7e37bed1e11be4b7e873b72bacdc909f
progmamun <mamun333089@gmail.com>
2022-04-21 14:02:49 +0600

    project-setup

---
+++ react-mongodb-crud/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import AddUser from './components/AddUser/AddUser';
+import './App.css';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="user/add" element={<AddUser></AddUser>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-mongodb-crud/src/components/AddUser/AddUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUser = () => {
+  return (
+    <div>
+      <h2>add user</h2>
+    </div>
+  );
+};
+
+export default AddUser;
