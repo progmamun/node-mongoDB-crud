87e1675696f91916da5ff430d7b1ee175f58b708
progmamun <mamun333089@gmail.com>
2022-04-21 16:21:30 +0600

    display-user

--- react-mongodb-crud/src/components/Home/Home.jsx
+++ react-mongodb-crud/src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/user')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
     <div>
-      <h3>home</h3>
+      <h3>Available Users</h3>
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>
+            {user.name}: {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
