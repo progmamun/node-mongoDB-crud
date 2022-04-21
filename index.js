b61986a748222398be74aa1533b6fe32e284a1c2
progmamun <mamun333089@gmail.com>
2022-04-21 13:05:13 +0600

    mongodb-insertOne

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,12 +17,20 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect(err => {
-  const collection = client.db('test').collection('devices');
-  console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db('foodExpress').collection('user');
+    const user = { name: 'Mamun Khan', email: 'dev@gmail.com' };
+    const result = await userCollection.insertOne(user);
+    console.log(`User inserted with id: ${result.insertedId}`);
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Running My Node CRUD Server');
